3dc94ceec573eaed0d01f270f230eb3e2d3fbfeb
GuJiuHan <1665302509@qq.com>
2019-10-17 10:23:57 +0800

    表字段声明及静态页面导入

--- src/net/togogo/servlet/LoginServlet.java
+++ src/net/togogo/servlet/LoginServlet.java
@@ -10,7 +10,19 @@ import java.io.IOException;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getMethod());
+        System.out.println(req.getParameter("userName"));
+        System.out.println(req.getParameter("password"));
+
+        String userName = req.getParameter("userName");
+        String pwd = req.getParameter("password");
+        //匹配帐号密码，匹配成功则跳转至登陆成功页面
+        if (null != userName && null != pwd){
+            String userName = req.getParameter("userName");
+        }
+
+
+        //失败则跳回登录页面
     }
 }
